3baa49603b5e8972ec8bc1f22bcee9193720d2af
ra2dd <radoslawpryw@gmail.com>
2023-11-05 12:29:12 +0100

    sort intros, slice data with empty names, add search functionality

--- components/StudentIntroList.tsx
+++ components/StudentIntroList.tsx
@@ -3,7 +3,7 @@ import { FC, useEffect, useState } from 'react'
 import { StudentIntro } from '../models/StudentIntro'
 import * as web3 from '@solana/web3.js'
 import { StudentIntroCoordinator } from '../utils/StudentIntroCoordinator'
-import { Center, HStack, Spacer, Button } from '@chakra-ui/react'
+import { Center, HStack, Spacer, Button, Input } from '@chakra-ui/react'
 
 const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
 const perPage = 10
@@ -12,14 +12,26 @@ export const StudentIntroList: FC = () => {
     const [studentIntros, setStudentIntros] = useState<StudentIntro[]>([])
     const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [page, setPage] = useState(1)
+    const [search, setSearch] = useState('novalue')
 
     useEffect(() => {
-        StudentIntroCoordinator.fetchPage(connection, page, perPage)
+        StudentIntroCoordinator.fetchPage(connection, page, perPage, search, search != 'novalue')
         .then(setStudentIntros)
-    }, [page])
+    }, [page, search])
     
     return (
         <div>
+            <Center>
+                <Input 
+                    id='search'
+                    color='gray.400'
+                    onChange={event => setSearch(event.target.value)}
+                    placeholder='Search'
+                    w='97%'
+                    mt={2}
+                    mb={2}
+                    />
+            </Center>
             {
                 studentIntros.map((studentIntro, i) => <Card key={i} studentIntro={studentIntro} /> )
             }
--- utils/StudentIntroCoordinator.ts
+++ utils/StudentIntroCoordinator.ts
@@ -1,25 +1,58 @@
 import * as web3 from '@solana/web3.js'
 import { StudentIntro } from '../models/StudentIntro'
+import bs58 from 'bs58'
 
 const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
 
 export class StudentIntroCoordinator {
     static accounts: web3.PublicKey[] = []
 
-    static async prefetchAccounts(connection: web3.Connection) {
+    static async prefetchAccounts(connection: web3.Connection, search: string) {
         const prefetchedAccounts = await connection.getProgramAccounts(
             new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID),
             {
-                dataSlice: { offset: 0, length: 0},
+                dataSlice: { offset: 1, length: 11},
+                filters: search === '' || search === 'novalue' ? [] : [
+                    {
+                        memcmp: 
+                            {
+                                offset: 5,
+                                bytes: bs58.encode(Buffer.from(search))
+                            }
+                    }
+                ]
             }
         )
 
-        this.accounts = prefetchedAccounts.map(account => account.pubkey)
+        prefetchedAccounts.sort((a,b) => {
+            const lengthA = a.account.data.readUInt32LE(0)
+            const lengthB = b.account.data.readUInt32LE(0)
+            const dataA = a.account.data.slice(4, 4 + lengthA)
+            const dataB = b.account.data.slice(4, 4 + lengthB)
+            return dataA.compare(dataB)
+        })
+
+        let blank = true
+        let counter = 0
+
+        // count empty names in account to slice it later
+        while (blank == true && counter < 30) {
+            const accountData = await connection.getAccountInfo(prefetchedAccounts[counter]?.pubkey)
+            // console.log(StudentIntro.deserialize(accountData?.data.name))
+            if (StudentIntro.deserialize(accountData?.data)?.name == '') {
+                counter++;
+            } else {
+                blank = false
+            }
+
+        }
+
+        this.accounts = prefetchedAccounts.slice(counter, prefetchedAccounts.length).map(account => account.pubkey)
     }
 
-    static async fetchPage(connection: web3.Connection, page: number, perPage: number): Promise<StudentIntro[]> {
-        if (this.accounts.length == 0) {
-            await this.prefetchAccounts(connection)
+    static async fetchPage(connection: web3.Connection, page: number, perPage: number, search: string, reload: boolean = false): Promise<StudentIntro[]> {
+        if (this.accounts.length == 0 || reload) {
+            await this.prefetchAccounts(connection, search)
         }
 
         const paginatedPublicKeys = this.accounts.slice(
@@ -41,7 +74,6 @@ export class StudentIntroCoordinator {
 
             return [...accum, studentIntro]
         }, [])
-
         return studentIntros
     }
 }
